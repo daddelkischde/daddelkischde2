fe2d3aa8d3df571d8219e152879735ac462131ba
Florian Geib <floppes@floppes.com>
2020-01-20 21:27:46 +0100

    Fan limits increased

--- fan_control_config.py
+++ fan_control_config.py
@@ -2,11 +2,11 @@
 
 # upper turn on limit in celsius
 # if the fan was off and the temperature is equal or higher than this limit, turn on the fan
-LIMIT_ON = 45.0
+LIMIT_ON = 52.0
 
 # lower turn off limit in celsius
 # if the fan was on and the temperature is equal or lower than this limit, turn off the fan
-LIMIT_OFF = 40.0
+LIMIT_OFF = 47.0
 
 # monitoring interval in seconds
 MONITOR_INTERVAL = 5
