47cd3aee7abcdfc899c1b50db49cefe4ea13d1ea
Florian Geib <floppes@floppes.com>
2020-10-04 21:01:54 +0200

    Use absolute paths

--- flash.sh
+++ flash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 port=/dev/ttyACM0
+scriptdir="/home/pi/daddelkischde2/"
 
 stop_service()
 {
@@ -28,13 +29,13 @@ stop_service dk2_safe_shutdown
 stop_service dk2_sercom_proxy
 
 echo "Resetting..."
-python reset.py $port
+python ${scriptdir}reset.py $port
 
 echo "Waiting for bootloader..."
 sleep 3
 
 echo "Flashing..."
-avrdude -v -p atmega32u4 -c avr109 -P $port -b 57600 -D -U flash:w:firmware/daddelkischde_mcu.ino.hex:i
+avrdude -v -p atmega32u4 -c avr109 -P $port -b 57600 -D -U flash:w:${scriptdir}firmware/daddelkischde_mcu.ino.hex:i
 
 echo "Waiting for reboot to finish..."
 sleep 20
